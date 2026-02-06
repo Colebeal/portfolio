390ef41ca9a430b228f31495162b39ad0dcbe00f
Colebeal <beal.cole@gmail.com>
2026-02-05 17:39:32 -0800

    change bio

--- src/components/Imagery/Menu/MenuContents.jsx
+++ src/components/Imagery/Menu/MenuContents.jsx
@@ -15,10 +15,9 @@ export default function MenuContents({
         <div className="flex flex-col 2xl:flex-row ">
           <h2 className="mb-3 w-[100%] pb-3 text-xl sm:pb-0">About me</h2>
           <p>
-            I am a twenty-something, now photography hobbyist, inspired
-            primarily by shadows and natural light. Traditionally trained as an
-            analogue photographer, I use this methodology of slow selective
-            shooting in my digital workflow.
+            With +10 years of experience as a working photographer, I seek out
+            clients who want create high quality, stylized imagery that will
+            drive business objectives.
           </p>
         </div>
         <ul className="mt-5 flex flex-col gap-3 2xl:flex-row">
@@ -42,14 +41,6 @@ export default function MenuContents({
                 cole@colebeal.com
               </span>
             </a>
-          </li>
-          <li className="flex gap-2">
-            <MdOutlineComputer className="text-2xl text-teal-900 dark:text-white" />
-            <Link to="../dev">
-              <span className="cursor-pointer hover:underline">
-                Web Development{" "}
-              </span>
-            </Link>
           </li>
         </ul>
         <div className="mt-8 h-[200px] bg-[url('/images/people/Portfolio-116.jpg')] bg-cover bg-center md:fixed md:bottom-32 md:right-0 md:w-[300px]"></div>
--- src/routes/AnimatedRoutes.jsx
+++ src/routes/AnimatedRoutes.jsx
@@ -2,7 +2,7 @@ import { useLocation, Routes, Route } from "react-router-dom"
 import { AnimatePresence } from "framer-motion"
 import Dev from "./Dev"
 import DevHome from "./DevHome"
-import App from "./App"
+// import App from "./App"
 import Imagery from "./Imagery"
 import DorffDesigns from "./DorffDesigns"
 import Bonsai from "./Bonsai"
@@ -13,14 +13,14 @@ const AnimatedRoutes = () => {
   return (
     <AnimatePresence mode="wait">
       <Routes location={location} key={location.pathname}>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<Imagery />} />
         <Route path="/dev" element={<Dev />}>
           <Route path="home" element={<DevHome />} />
           <Route path="dorff-designs" element={<DorffDesigns />} />
           <Route path="bonsai" element={<Bonsai />} />
         </Route>
 
-        <Route path="/imagery" element={<Imagery />} />
+        {/* <Route path="/imagery" element={<Imagery />} /> */}
       </Routes>
     </AnimatePresence>
   )
